dff4e55facb1484e8665aacbab463b0ca68426fd
jeffJeffrey <jelfnguimdo@gmail.com>
2025-08-26 21:04:25 +0100

    remove dockerfile

--- src/main/java/com/eduDataCam/EDUDATACAM/config/WebConfig.java
+++ src/main/java/com/eduDataCam/EDUDATACAM/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://192.168.0.146:3000", "https://edudatacam-app.vercel.app")  // Ajoutez l'origine de votre frontend
+                        .allowedOrigins("https://edudatacam-app.vercel.app")  // Ajoutez l'origine de votre frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
 
     }
